993655673b2c3c51351ac868073566a1dee9d573
kbartczak <konrad.bartczak@sartoirs.com>
2022-07-19 19:53:19 +0200

    Make a mess.

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -7,5 +7,7 @@ import com.example.plugins.*
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
+        configureRouting()
+        configureRouting()
     }.start(wait = true)
 }
